40bf8f9ce9ff889f77b4f6af117972ce072eecf4
Harshit Bhoraskar <harshit.bhoraskar@gmail.com>
2022-05-08 14:41:45 -0700

    update query

--- Backend/controllers/customerSupportController.js
+++ Backend/controllers/customerSupportController.js
@@ -2,6 +2,7 @@ import con from  '../index.js';
 import { sendCustomError, sendCustomSuccess, sendInternalServerError } from './common.js';
 
 export const updateCustomerSupport = (req, res)  => {
+  console.log("here line 5");
   try{
 
     const {
@@ -11,26 +12,23 @@ export const updateCustomerSupport = (req, res)  => {
       type
     } =  req.body;
 
-    const updateCustomerSupport = `CREATE TABLE IF NOT EXISTS customerSupport (
-userEmail VARCHAR(500),
-userName VARCHAR(500),
-message VARCHAR(500),
-type VARCHAR(500),
-);
-INSERT INTO customerSupport (userEmail,userName,message,type) VALUES (?,?,?,?)`;
+    const updateCustomerSupport = `INSERT INTO customerSupport (userEmail,userName,message,type) VALUES(?,?,?,?)`;
 
-con.query(updateCustomerSupport,[],[
+  // const updateCustomerSupport = "CREATE TABLE customerSupport(userEmail VARCHAR(500),userName VARCHAR(500),message VARCHAR(500),type VARCHAR(500))"
+con.query(updateCustomerSupport,[
   userEmail,
   userName,
   message,
   type
 ],(err, result) => {
+
   if(result){
       sendCustomSuccess(res, { data: result});
   }
   else{
       sendCustomError(res, 404, 'Entity Not Found');
   }})
+
   }
   catch(err){
       sendInternalServerError(res);
