276352bc32f08c62146e54d19072ad5e8186c861
Harshit Bhoraskar <harshit.bhoraskar@gmail.com>
2022-05-15 14:29:10 -0700

    added ridedetails router

--- Backend/index.js
+++ Backend/index.js
@@ -2,17 +2,28 @@
 import dotenv from 'dotenv'
 import { createConnection } from 'mysql';
 import express from 'express';
+const mongoose = require("mongoose");
+
+// const MongoClient = require("mongodb").MongoClient;
+
 
 import userRouter from './routes/userRoutes.js';
 import carRouter from  './routes/carRoutes.js';
 import rideRouter from './routes/rideRoutes.js';
 import customerSupportRouter from './routes/customerSupportRoutes.js';
+import rideDetailsRouters from './routes/rideDetailsRouters.js';
+import cors from 'cors';
 
 dotenv.config();
 var app = express();
 app.use(express.json());
 
-import cors from 'cors';
+// const uri ="mongodb+srv://cmpe281:cmpe281@cluster0.ukbu6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+// const client = new MongoClient(uri);
+mongoose.connect("mongodb+srv://cmpe281:cmpe281@cluster0.ukbu6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{useNewUrlParser:true})
+const connection  = mongoose.connection;
+
+
 const corsConfig = {
   credentials: true,
   origin: true,
@@ -35,5 +46,6 @@ app.use('/user', userRouter);
 app.use('/car', carRouter);
 app.use('/ride', rideRouter);
 app.use('/customerSupport', customerSupportRouter);
+app.use('/rideDetails', rideDetailsRouters);
 
 export default con;
