8a33657f50513ec66719ec34adce75ef25e48186
Harshit Bhoraskar <harshit.bhoraskar@gmail.com>
2022-05-16 23:46:33 -0700

    changed base url from localhost to ip

--- Frontend/src/services/constants.js
+++ Frontend/src/services/constants.js
@@ -1,3 +1,4 @@
 //export const BACKEND_URL='http://ec2-3-14-43-170.us-east-2.compute.amazonaws.com';
-export const BACKEND_URL='http://localhost';
+// export const BACKEND_URL='http://localhost';
+export const BACKEND_URL='40.77.108.208';
 export const BACKEND_PORT='3001';
